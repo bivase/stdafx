f30c5b343fc13d0b86d583a65515121a9e89d1d4
bivase <70434291+bivase@users.noreply.github.com>
2020-08-30 09:39:17 +0300

    добалены файлы stdafx.h, stdafx.cpp

---
+++ stdafx.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <array>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <numeric>
+#include <random>
+#include <functional>
+#include <string>
+#include <filesystem>
+#include <optional>
+#include <map>
+#include <execution>
+
+#include "BitsOperations.hpp"
+#include "idxs.hpp"
+#include "dvbs2.hpp"
+#include "types.h"
+
+namespace fs = std::filesystem;
