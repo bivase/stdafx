9af49003d38a3fa98e2c3a4efbdf4eb36106fd8d
bivase <bivase@bk.ru>
2020-09-09 04:21:18 +0300

    добавлен QFileDialog, удален execution

--- stdafx.h
+++ stdafx.h
@@ -1,4 +1,8 @@
 #pragma once
+#if defined __has_include && __has_include ("QMainWindow")
+#include <QFileDialog>
+#endif
+
 #include <array>
 #include <algorithm>
 #include <iostream>
@@ -11,11 +15,10 @@
 #include <filesystem>
 #include <optional>
 #include <map>
-#include <execution>
+//#include <execution>
 
-#include "BitsOperations.hpp"
-#include "idxs.hpp"
-#include "dvbs2.hpp"
-#include "types.h"
+#include "bits_operations/bits_operations.hpp"
+#include "idxs/idxs.hpp"
+#include "dvbs2_mono/dvbs2_mono.hpp"
 
 namespace fs = std::filesystem;
