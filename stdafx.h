eb7693bd4b601385c5fea14bb9652b1aa8aef485
bivase <bivase@bk.ru>
2020-09-21 03:09:57 +0300

    добавлен random, random.hpp

--- stdafx.h
+++ stdafx.h
@@ -17,8 +17,10 @@
 #include <optional>
 #include <map>
 #include <iterator>
+#include <random>
 //#include <execution>
 
 #include "bits_operations/bits_operations.hpp"
 #include "idxs/idxs.hpp"
+#include "random/random.hpp"
 #include "dvbs2_mono/dvbs2_mono.hpp"
